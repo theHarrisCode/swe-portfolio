23525ff41c49aa8d760e9bed4b481a025df6ccbd
Darren Harris <theharriscode401@gmail.com>
2024-09-25 02:17:53 -0400

    adding font text link & changing to .less

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./NavBar.styles.css"
+import "./NavBar.styles.less"
 import "../../index.css"
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -16,41 +16,41 @@ const navigation = [
     { name: 'Resume', href: 'https://www.google.com', current: false },
     { name: 'Contact Me', href: 'https://www.google.com', current: false },
     { name: 'test', href: 'https://www.nba.com', current: true },
-
 ]
 
 
 function NavBar() {
     return (
         <>
-            <Disclosure as="nav" className="bg-gray-800 rounded-2xl shadow-2xl mt-6 border-black nav-bar">
-            <div className="text-white flex flex-1">Darren Harris</div>
-                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                {/* Mobile menu button*/}
-                                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                    <span className="absolute -inset-0.5" />
-                                    <span className="sr-only">Open main menu</span>
-                                    <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
-                                    <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
-                                </DisclosureButton>
-                            </div>
-                            {/* <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"> */}
-                                {/* <div className="hidden sm:ml-6 sm:block"> */}
-                                    <div className="flex space-x-4">
-                                        {navigation.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                aria-current={item.current ? 'page' : undefined}
-                                                className={classNames(
-                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                    'rounded-md px-3 py-2 text-sm font-medium',
-                                                )}
-                                            >
-                                                {item.name}
-                                            </a>
-                                        ))}
-                                </div>
+            <div className="mx-auto text-white mt-5 header-title-text">theHarrisCode</div>
+            <Disclosure as="nav" className="bg-gray-800 rounded-2xl shadow-2xl mt-6 border-black nav-bar flex justify-between ">
+                <div className="text-white flex ">Darren Harris</div>
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                    {/* Mobile menu button*/}
+                    <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                        <span className="absolute -inset-0.5" />
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
+                        <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
+                    </DisclosureButton>
+                </div>
+                {/* <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"> */}
+                {/* <div className="hidden sm:ml-6 sm:block"> */}
+                <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                        <a
+                            key={item.name}
+                            href={item.href}
+                            aria-current={item.current ? 'page' : undefined}
+                            className={classNames(
+                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                'rounded-md px-3 py-2 text-sm font-medium',
+                            )}
+                        >
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
 
                 <DisclosurePanel className="sm:hidden">
                     <div className="space-y-1 px-2 pb-3 pt-2">
