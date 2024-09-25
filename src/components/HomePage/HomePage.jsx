a2f8728c63a51e19ef62a9d22d0744c2c94d2c7b
Darren Harris <theharriscode401@gmail.com>
2024-09-25 01:25:06 -0400

    adding the nav bar components, headless ui navbar, tailwind syntax

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import "./HomePage.styles.css"
+
 function HomePage(){
     return(
         <>
-            <NavBar />
-            <div>this is a test</div>
+            <div class="home-page-container">
+                <NavBar />
+            </div>
         </>
     )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,75 +1,74 @@
 import React from "react";
-import { DownOutlined, SmileOutlined } from "@ant-design/icons";
-import { Dropdown, Space } from "antd";
+import "./NavBar.styles.css"
+import "../../index.css"
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const items = [
-    {
-        key: 1,
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.google.com">
-                About
-            </a>
-        ),
-        icon: <SmileOutlined/>
-    },
-    {
-        key: 2,
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.google.com">
-                Skills
-            </a>
-        ),
-        icon: <SmileOutlined/>
-    },
-    {
-        key: 3,
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.google.com">
-                Projects
-            </a>
-        ),
-        icon: <SmileOutlined/>
-    },
-    {
-        key: 4,
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.google.com">
-                Resume
-            </a>
-        ),
-        icon: <SmileOutlined/>
-    },
-    {
-        key: 5,
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.google.com">
-                Contact Me
-            </a>
-        ),
-        icon: <SmileOutlined/>
-    }
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+
+const navigation = [
+    { name: 'About', href: 'https://www.google.com', current: true },
+    { name: 'Skills', href: 'https://www.google.com', current: true },
+    { name: 'Projects', href: 'https://www.google.com', current: false },
+    { name: 'Resume', href: 'https://www.google.com', current: false },
+    { name: 'Contact Me', href: 'https://www.google.com', current: false },
 ]
 
 
-function NavBar(){
-    return(
+function NavBar() {
+    return (
         <>
-            <div>
-                <h2>Darren Harris</h2>
-                <p></p>
-            </div>
-            <div style={ {
-                textAlign: 'center'
-            } }>
-                <Dropdown 
-                    menu={ {items} }
-                >
-                    <Space>
-                        Get To Know Me
-                        <DownOutlined/>
-                    </Space>
-                </Dropdown>
-            </div>
+            <Disclosure as="nav" className="bg-gray-800 rounded-2xl shadow-2xl mt-6 border-black nav-bar">
+            <div className="text-white flex flex-1">Darren Harris</div>
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">Open main menu</span>
+                                    <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
+                                    <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
+                                </DisclosureButton>
+                            </div>
+                            {/* <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"> */}
+                                {/* <div className="hidden sm:ml-6 sm:block"> */}
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                aria-current={item.current ? 'page' : undefined}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'rounded-md px-3 py-2 text-sm font-medium',
+                                                )}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                </div>
+
+                <DisclosurePanel className="sm:hidden">
+                    <div className="space-y-1 px-2 pb-3 pt-2">
+                        {navigation.map((item) => (
+                            <DisclosureButton
+                                key={item.name}
+                                as="a"
+                                href={item.href}
+                                aria-current={item.current ? 'page' : undefined}
+                                className={classNames(
+                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                    'block rounded-md px-3 py-2 text-base font-medium',
+                                )}
+                            >
+                                {item.name}
+                            </DisclosureButton>
+                        ))}
+                    </div>
+                </DisclosurePanel>
+            </Disclosure>
         </>
     )
 }
