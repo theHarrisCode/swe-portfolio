9e25e7a73a38868b2d1a6114f5e11f953a40defa
Darren Harris <theharriscode401@gmail.com>
2024-09-29 05:29:48 -0400

    Updating Home page font style and small text edits

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import darren_photo from "../../assets/images/DH-Hack-2024.jpeg";
 import "./Home.styles.less";
+import Typewriter from "../Typewriter/Typewriter";
+
 
 function Home() {
     return (
         <>
-            <div className="mt-10 flex about-me-container">
+            <div className="mt-6 flex about-me-container">
                 <div className="w-1/2 p-10">
                     <img
                         src={darren_photo}
@@ -13,10 +15,9 @@ function Home() {
                         className="rounded-3xl"
                     />
                 </div>
-                <div className="w-1/2 p-10">
-                    <p className="text-4xl leading-snug intro-text">
-                        Hello! I'm Darren Harris, Software Developer with an expertise in web development and a
-                        specialization in Front End Development. I have strong interest in learning Backend Development and Artificial Predictive Modeling & Machine Learning. </p>
+                <div className="w-1/2 p-10 text-4xl leading-snug intro-text-container">
+                    <Typewriter className=" intro-text" text="I 'm Darren Harris, Software Developer with an expertise in web development and a
+                        specialization in Front End Development. I have strong interest in learning Backend Development and Artificial Predictive Modeling & Machine Learning." speed={ 50 }/>
                 </div>
             </div>
         </>
