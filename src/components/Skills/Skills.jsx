d4698354cf0e7854e5adb1f427cb1e68e2c98ac9
Darren Harris <theharriscode401@gmail.com>
2024-10-04 11:10:59 -0400

    Skills files

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,16 @@
+// import {useState } from 'react';
+
+
+export default function Skills( { headerStyles } ){
+    return (
+        <>
+            <>
+                <div className="min-w-full">
+                    <div className={ headerStyles.divClass }>
+                        <h1 className={ headerStyles.h1Class }>Skills</h1>
+                    </div>
+                </div>
+            </>
+        </>
+    )
+}
