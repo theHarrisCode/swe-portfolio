116f03de2e6fcdccb716bca2098c18a70226de17
Darren Harris <theharriscode401@gmail.com>
2024-11-07 14:39:38 -0500

    Removing Mobile nav bar setup, adding id properties to nav array

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./NavBar.styles.less"
 import "../../index.css"
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -10,11 +9,11 @@ function classNames(...classes) {
 
 
 const navigation = [
-    { name: 'Who Am I ?', href: 'https://www.google.com', current: false },
-    { name: 'Skills', href: 'https://www.google.com', current: false },
-    { name: 'Projects', href: 'https://www.google.com', current: false },
-    { name: 'Resume', href: 'https://www.google.com', current: false },
-    { name: 'Contact Me', href: 'https://www.google.com', current: false },
+    { name: 'Who Am I ?', href: 'https://www.google.com', current: false, id: 'about-section'},
+    { name: 'Skills', href: 'https://www.google.com', current: false, id: 'skills-section' },
+    { name: 'Projects', href: 'https://www.google.com', current: false, id: 'projects-section' },
+    { name: 'Resume', href: 'https://www.google.com', current: false, id: 'resume-section'},
+    { name: 'Contact Me', href: 'https://www.google.com', current: false, id: 'contact-section' },
 ]
 
 
@@ -23,17 +22,6 @@ function NavBar() {
         <>
             <Disclosure as="nav" className="bg-black rounded-2xl shadow-2xl my-6 border-black flex justify-between nav-bar">
                 <div className="text-white flex tracking-wider nav-bar-title">Darren Harris</div>
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    {/* Mobile menu button*/}
-                    <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                        <span className="absolute -inset-0.5" />
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
-                        <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
-                    </DisclosureButton>
-                </div>
-                {/* <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"> */}
-                {/* <div className="hidden sm:ml-6 sm:block"> */}
                 <div className="flex space-x-4">
                     {navigation.map((item) => (
                         <a
