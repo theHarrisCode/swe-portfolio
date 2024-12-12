6f5a35c5e79579a7a03c52187cda8058240a6652
Darren Harris <theharriscode401@gmail.com>
2024-12-11 19:48:14 -0500

    Add Modal Changes Including:
    * Importing Parent props
    * uploading images to storage collection
    * downloading and listing images from storage collection
    * adding document to database
    * adding comments

--- src/components/Modals/AddModal.jsx
+++ src/components/Modals/AddModal.jsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { useState, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react';
 import Box from '@mui/material/Box';
 import { Button } from "@radix-ui/themes";
 import Modal from '@mui/material/Modal';
-import { PlusIcon } from "@radix-ui/react-icons"
-import './AddModal.styles.less'
-import { firestore } from "../../Firebase"
-import { addDoc, collection } from "@firebase/firestore"
+import { PlusIcon } from "@radix-ui/react-icons";
+import './AddModal.styles.less';
+import { firestore } from "../../Firebase";
+import { addDoc, collection } from "@firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage"
+import { Cone } from 'lucide-react';
+
 
 /* Modal styling */
 const style = {
@@ -21,85 +24,122 @@ const style = {
   p: 4,
 };
 
-export default function AddModal({ sendToParentData }) {
-  const [isVisible, setIsVisible] = useState(true);
-  const [file, setFile] = useState()
+/* Firebase Storage for image */
+const imageStorage = getStorage();
+
+export default function AddModal({ sendToParentData, colLength, changeSubmitBool }) {
+  const [photo, setPhoto] = useState([]);
+  const [file, setFile] = useState('');
+
+  const storageRef = ref(imageStorage, `images/${file.name}`);
 
   /* Handling input file in project addition */
-  const handleInputFile = (e) => {
-    console.log(e.target.files)
-    setFile(URL.createObjectURL(e.target.files[0]))
+  const handleInputPhoto = async (e) => {
+    setFile(e.target.files[0]);
   }
+  console.log("Photos: ", photo)
 
   /* Handling open and close function of modal */
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  
-  /* Handling saving project data */
+
+  /* Handling saving project data to database */
   const handleSave = async (e) => {
     e.preventDefault();
 
-    let projectData = {
-      name: nameRef.current.value,
-      photo: photoRef.current.value,
-      description: desRef.current.value,
-      link: linkRef.current.value
-    }
+    /* Uploads picture to firebase storage */
+    uploadBytes(storageRef, file)
+      .then((snapshot) => {
+        console.log("Image Uploaded", snapshot.name);
+      },
+        (err) => {
+          console.log("Error: ", err)
+        }
+      );
 
-    try{
-      addDoc(dbRef, projectData)
+    /* Downloading pictures from storage and updating photo state variable */
+    listAll(ref(imageStorage, 'images/'))
+      .then(imgs => {
+        imgs.items.forEach(img => {
+          getDownloadURL(img).then(url => {
+            setPhoto([...photo, url]);
+            console.log("Collection Length: ", colLength)
+            console.log("Photos at first element: ", photo)    
+            console.log("Photos at colLength element: ", photo[colLength])
+          })
+        })
+      })
+
+    /* Checking collection length, max is 5 */
+    console.log("Collection Length: ", colLength)
+    console.log("Photos at first element: ", photo[0])    
+    console.log("Photos at colLength element: ", photo[colLength])
+    if (colLength <= 5) {
+      let projectData = {
+        name: nameRef.current.value,
+        photo: photo,
+        description: desRef.current.value,
+        link: linkRef.current.value
+      };
+
+      try {
+        addDoc(dbRef, projectData);
+      }
+      catch (err) {
+        console.error(err);
+      }
+      changeSubmitBool = true;
     }
-    catch(err){
-      console.error(err);
+    else {
+      alert("You can only add 5 projects.");
     }
-  }
+    setOpen(false)
+  };
 
   /* Database Reference */
   const dbRef = collection(firestore, 'Projects');
 
   /* Reference for each input value */
   const nameRef = useRef();
-  const photoRef = useRef()
+  const photoRef = useRef();
   const desRef = useRef();
   const linkRef = useRef();
 
   return (
     <div>
-      {isVisible && (<div>
-        <Button color="gray" id="add-btn" variant="outline" onClick={ handleOpen }>
-          <PlusIcon /> Add
-        </Button>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <form onSubmit={ handleSave }> 
-              <div className='input-container'>
-                <label className='add-modal-label'>Project Name</label>
-                <input name='project-name' placeholder='Project Name' type='text' className='pswrd-input' ref={nameRef}/>
-              </div>
-              <div className='input-container'>
-                <label className='add-modal-label'>Photo</label>
-                <input name='project-password' type="file" onChange={ handleInputFile } ref={photoRef}/>
-              </div>
-              <div className='input-container'>
-                <label className='add-modal-label'>Project Description</label>
-                <textarea name="prj-area" id="prj-area" rows="15" cols="32" ref={desRef}></textarea>
-              </div>
-              <div className="input-container">
-                <label className="add-modal-label">Project Link</label>
-                <input name="project-link" type="text" className='pswrd-input' ref={linkRef}/>
-              </div>
-              <Button variant='outline' color='gray' id='sbmt-btn' type='submit'>Submit</Button>
-            </form>
-          </Box>
-        </Modal>
-      </div>
-      )}
+      <Button color="gray" id="add-btn" variant="outline" onClick={handleOpen}>
+        <PlusIcon /> Add
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form onSubmit={handleSave}>
+            <div className='input-container'>
+              <label className='add-modal-label'>Project Name</label>
+              <input name='project-name' placeholder='Project Name' type='text' className='pswrd-input' ref={nameRef} />
+            </div>
+            <div className='input-container'>
+              <label className='add-modal-label'>Photo</label>
+              <input name='project-picture' type="file" onChange={handleInputPhoto} ref={photoRef} />
+            </div>
+            <div className='input-container'>
+              <label className='add-modal-label'>Project Description</label>
+              <textarea name="prj-area" id="prj-area" rows="15" cols="32" ref={desRef}></textarea>
+            </div>
+            <div className="input-container">
+              <label className="add-modal-label">Project Link</label>
+              <input name="project-link" type="text" className='pswrd-input' ref={linkRef} />
+            </div>
+            <Button variant='outline' color='gray' id='sbmt-btn' type='submit'
+            >Submit</Button>
+          </form>
+        </Box>
+      </Modal>
     </div>
   );
 }
