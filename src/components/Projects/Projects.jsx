fb504be92cbeb1af765fdec4b13160e3ae9cf4c8
Darren Harris <theharriscode401@gmail.com>
2024-12-11 19:53:53 -0500

    Project Component Update
    * Creating State Variables
    * Fetching Data from firestore database and displaying
    * Adding comments
    * Note: This version has the remove visibility and the cancel button functions unfinished

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,19 +1,44 @@
 import "./Projects.styles.less"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Button } from "@radix-ui/themes";
 import { TrashIcon } from "@radix-ui/react-icons"
-import itinerary_photo from "../../assets/images/project-photos/itinerary-snapshot.png"
 import EditModal from "../Modals/EditModal.jsx"
 import AddModal from "../Modals/AddModal.jsx"
-
+import { collection, onSnapshot } from "firebase/firestore"
+import { firestore } from "../../Firebase.js";
 
 function Projects({ headerStyles }) {
 
+    /* State Variables */
     const [isVisible, setIsVisible] = useState(false);
+    const [colSize, setColSize] = useState(0);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [data, setData] = useState([])
+    const [singleRemoveVisibility, setSingleRemoveVisibility] = useState(false);
 
     const handleProjectVisibility = (data) => {
         setIsVisible(data);
     }
+
+    const handleSingleRemoveVisibility = () => {
+        setSingleRemoveVisibility(true);
+    }
+
+    const handleCancel = () => {}
+    
+    /* Fetching Project snapshot from firestore database */
+    useEffect(() => {
+        const fetchData = async () => {
+            const colRef = collection(firestore, "Projects");
+            onSnapshot(colRef, (snapshot) => {
+                setColSize(snapshot.size)
+                const data = snapshot.docs.map((doc) => doc.data())
+                setData(data)
+            } )
+        }
+
+        fetchData();
+    }, [])
 
     return (
         <>
@@ -23,28 +48,30 @@ function Projects({ headerStyles }) {
                 </div>
                 <div className="prj-btn-container">
                         <EditModal sendToParentData = { handleProjectVisibility }/>
-                    {isVisible && (<div className="flex">
-                        <AddModal/>
-                        <Button color="red" id="rmv-btn" variant="outline">
+                    { isVisible && (<div className="flex">
+                        <AddModal colLength={colSize} changeSubmitBool={setIsSubmitted}/>
+                        <Button color="red" id="rmv-btn" variant="outline" onClick={ handleSingleRemoveVisibility }>
                             <TrashIcon /> Remove
                         </Button>
+                        { singleRemoveVisibility && (
+                            <Button color="red" variant="outline" onClick={ handleCancel }> Cancel </Button>
+                        )}
                     </div>
                 )}
                 </div>
-                <div className="flex p-16 justify-between prj-outer-container">
-                    <div id="first-prj" className="prj-container">
-                        <div className="prj-title">Birthday Itinerary</div>
-                        <div className="img-container">
-                            <img src={ itinerary_photo } alt="b-day-project" />
+                <div className="flex flex-1 p-16 justify-evenly prj-outer-container">
+                { data.map( (prj) => {
+                    return (
+                        <div id="first-prj" className="prj-container">
+                            <div className="prj-title" key={prj.name}>{ prj.name }</div>
+                            <div className="img-container">
+                                <img src={ prj.photo} alt="b-day-project" key={prj.photo}/>
+                            </div>
                         </div>
-                    </div>
-                    <div id="second-prj" className="prj-container">
-                        <div className="prj-title">Patient Tracking Web App</div>
-                    </div>
-                    <div id="third-prj" className="prj-container">
-                        <div className="prj-title">StatPad</div>
-                    </div>
-                </div>
+                    )
+                }) 
+                }
+                </div> 
             </div>
         </>
     )
